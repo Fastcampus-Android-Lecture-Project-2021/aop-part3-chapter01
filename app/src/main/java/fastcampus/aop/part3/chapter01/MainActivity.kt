303882bce84e7e65301062f1a02ecba8f870c11e
devussy <y.devussy@gmail.com>
2021-04-11 20:18:33 +0900

    Initial commit

---
+++ app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -0,0 +1,53 @@
+package fastcampus.aop.part3.chapter01
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
+    }
+}
---
+++ app/src/main/java/fastcampus/aop/part3/chapter01/MyFirebaseMessagingService.kt
@@ -0,0 +1,113 @@
+package fastcampus.aop.part3.chapter01
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        // Apps that use Firebase Cloud Messaging should implement onNewToken()
+        // in order to observe token changes
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        createNotificationChannelIfNeeded()
+
+        val type = remoteMessage.data["type"]
+            ?.let { NotificationType.valueOf(it) }
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+        type ?: return
+
+        NotificationManagerCompat.from(this)
+            .notify(type.id, createNotification(type, title, message))
+    }
+
+    private fun createNotificationChannelIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(
+        type: NotificationType,
+        title: String?,
+        message: String?
+    ): Notification {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("notificationType", "${type.title} 타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, type.id, intent, FLAG_UPDATE_CURRENT)
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        when (type) {
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(
+                            "😀 😃 😄 😁 😆 😅 😂 🤣 🥲 ☺️ 😊 😇 " +
+                                    "🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 " +
+                                    "😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 " +
+                                    "😞 😔 😟 😕 🙁 ☹️ 😣 😖 😫 😩 🥺 😢 " +
+                                    "😭 😤 😠 😡 🤬 🤯 😳 🥵 🥶 😱 😨 😰 " +
+                                    "😥 😓 🤗 🤔 🤭 🤫 🤥 😶 😐 😑 😬 🙄 " +
+                                    "😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 🤐 🥴 " +
+                                    "🤢 🤮 🤧 😷 🤒 🤕"
+                        )
+                )
+            }
+            NotificationType.CUSTOM -> {
+                notificationBuilder
+                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                    .setCustomContentView(
+                        RemoteViews(
+                            packageName,
+                            R.layout.view_custom_notification
+                        ).apply {
+                            setTextViewText(R.id.title, title)
+                            setTextViewText(R.id.message, message)
+                        }
+                    )
+            }
+        }
+
+        return notificationBuilder.build()
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
+    }
+}
